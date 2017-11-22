507c83d608760280f930b13c6fbcf78fddd621dc
sire_marcos <sire_marcos@marcoss-imac.local>
2017-11-22 02:25:14 -0500

    Initial Commit

---
+++ Evaluation+/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Evaluation+
+//
+//  Created by Marcos Gomes on 17-11-22.
+//  Copyright © 2017 Concordia Continuing Education (CCE). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
