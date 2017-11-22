fb1a7986a07a990daba68233c607c5ee74ec0516
sire_marcos <sire_marcos@marcoss-imac.local>
2017-11-22 03:08:48 -0500

    working

---
+++ Evaluation+/SaveLoadMenager.swift
@@ -0,0 +1,41 @@
+/*
+
+Class to save and load files
+
+*/
+
+//-- Libraries --
+import Foundation
+//---------------
+
+class SaveLoadMenager
+{
+	//-- If exist the file --
+	func checkExistingSaves(fileName: String) -> Bool
+	{
+		if UserDefaults.standard.object(forKey: fileName) == nil
+		{
+			return false
+		}
+		return true
+	}
+	//-----------------------
+	//---- Save the file ----
+	func save(theData: AnyObject, fileName: String)
+	{
+		UserDefaults.standard.set(theData, forKey: fileName)
+	}
+	//-----------------------
+	//----- Delete file -----
+	func deleteFile(fileName: String)
+	{
+		UserDefaults.standard.removeObject(forKey: fileName)
+	}
+	//-----------------------
+	//------ Load file ------
+	func loadData(fileName: String) -> AnyObject
+	{
+		return UserDefaults.standard.object(forKey: fileName) as AnyObject
+	}
+	//-----------------------
+}
--- Evaluation+/SignIn.swift
+++ Evaluation+/SignIn.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignIn: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
