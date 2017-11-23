34d35e98c513d6b762f127932badc4e8877f3796
eleves <eleves@marcospaulo.local>
2017-11-23 16:33:13 -0500

    working

--- Evaluation+/SignIn.swift
+++ Evaluation+/SignIn.swift
@@ -1,25 +1,97 @@
-//
-//  ViewController.swift
-//  Evaluation+
-//
-//  Created by Marcos Gomes on 17-11-22.
-//  Copyright © 2017 Concordia Continuing Education (CCE). All rights reserved.
-//
+/*
+  ViewController.swift
+  Evaluation+
 
+  Created by Marcos Gomes on 17-11-22.
+  Copyright © 2017 Concordia Continuing Education (CCE). All rights reserved.
+*/
+
+//---------- Libraries ----------
 import UIKit
+//-------------------------------
 
-class SignIn: UIViewController {
 
-	override func viewDidLoad() {
+class SignIn: UIViewController
+{
+	//----------- Outlets -----------
+	@IBOutlet weak var imgViewLogo: UIImageView!
+	@IBOutlet weak var labelTitle: UILabel!
+	@IBOutlet weak var labelQuestion: UILabel!
+	
+	@IBOutlet weak var viewSignIn: UIView!
+	@IBOutlet weak var viewSignUp: UIView!
+	
+	@IBOutlet weak var fieldUsername: UITextField!
+	@IBOutlet weak var fieldPassword: UITextField!
+	
+	@IBOutlet weak var buttonSignIn: UIButton!
+	@IBOutlet weak var buttonCreateAccount: UIButton!
+	
+	//-------------------------------
+	
+	//----------- var/let -----------
+	var username: String!
+	var password: String!
+	//-------------------------------
+
+	override func viewDidLoad()
+	{
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		
+		//----------- Classes -----------
+		let style = Styles()
+		//-------------------------------
+		
+		
+		
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	@IBAction func signIn(_ sender: UIButton)
+	{
+		let checkLoad = SaveLoadMenager()
+		
+		username = fieldUsername.text!
+		password = fieldPassword.text!
+		
+		if checkLoad.checkExistingSaves(fileName: username) == true
+		{
+			let userData = checkLoad.loadData(fileName: username) as! (Int, String, String)
+			
+			if userData.2 == password
+			{
+				//code to pass to the next view
+			}
+		}
+		else
+		{
+			//alert
+		}
+		
+		
+		
 	}
+	
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
